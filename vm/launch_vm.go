984b9f8ab1448230ebcc90787acaf981f59a0b12
kuro337 <cb.kalz@gmail.com>
2024-03-02 00:47:50 -0500

    libvirt net

--- vm/launch_vm.go
+++ vm/launch_vm.go
@@ -11,20 +11,17 @@ import (
 
 /* Launches a new Ubuntu VM with nothing setup */
 func LaunchNewVM(vmConfig *VMConfig) (*VMConfig, error) {
-	utils.LogMainAction(fmt.Sprintf("Launching new VM %s : %d mem %d vcpu", vmConfig.VMName, vmConfig.Memory, vmConfig.CPUCores))
+	LogLaunchInit(vmConfig.VMName, vmConfig.Memory, vmConfig.CPUCores)
 
 	vmConfig.PullImage()
-
-	utils.LogSection("CREATING BASE IMAGE")
 
 	if err := vmConfig.CreateBaseImage(); err != nil {
 		utils.LogError(fmt.Sprintf("Failed to Setup VM ERROR:%s", err))
 		Cleanup(vmConfig.VMName)
 		return nil, err
 	}
-	log.Printf("Modified Base Image Creation Success at %s", vmConfig.ImagesDir)
 
-	utils.LogSection("SETTING UP VM")
+	fmt.Printf(utils.LogSection("SETTING UP VM"))
 
 	if err := vmConfig.SetupVM(); err != nil {
 		utils.LogError(fmt.Sprintf("Failed to Setup VM ERROR:%s", err))
@@ -33,7 +30,7 @@ func LaunchNewVM(vmConfig *VMConfig) (*VMConfig, error) {
 
 	}
 
-	utils.LogSection("GENERATING CLOUDINIT USERDATA")
+	fmt.Printf(utils.LogSection("GENERATING CLOUDINIT USERDATA"))
 
 	if err := vmConfig.GenerateCloudInitImgFromPath(vmConfig.UserData); err != nil {
 		utils.LogError(fmt.Sprintf("Failed to Generate Cloud-Init Disk ERROR:%s", err))
@@ -41,15 +38,11 @@ func LaunchNewVM(vmConfig *VMConfig) (*VMConfig, error) {
 		return nil, err
 	}
 
-	log.Printf("Successfully Created Cloud-Init user-data .img file: %s", vmConfig.UserData)
-
-	utils.LogSection("LAUNCHING VM")
+	fmt.Printf(utils.LogSection("LAUNCHING VM"))
 
 	if err := vmConfig.CreateVM(); err != nil {
 		utils.LogError(fmt.Sprintf("Failed to Create VM ERROR:%s", err))
 		log.Printf("Check sudo cat /var/log/libvirt/qemu/%s.log for verbose failure logs", vmConfig.VMName)
-		log.Printf("Known Issue: Ensure no invalid hooks are present in /etc/libvirt/hooks/qemu")
-
 		return nil, err
 
 	}
@@ -61,7 +54,17 @@ func LaunchNewVM(vmConfig *VMConfig) (*VMConfig, error) {
 
 	slog.Info("VM created successfully")
 
-	utils.LogBold("For VM Boot Logs: Check /var/log/cloud-init-output.log to view boot logs.")
+	fmt.Printf(utils.TurnBold("For VM Boot Logs: Check /var/log/cloud-init-output.log to view boot logs."))
 
 	return vmConfig, nil
+}
+
+func LogLaunchInit(vmName string, mem, cores int) {
+	fmt.Println(utils.LogMainAction(fmt.Sprintf("Launching new VM %s : %d mem %d vcpu",
+		vmName,
+		mem,
+		cores,
+	)))
+
+	utils.LogSection("CREATING BASE IMAGE")
 }
