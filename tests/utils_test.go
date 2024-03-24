f5cff202c46168c31478034a5a2aa5b201e108b4
kuro337 <cb.kalz@gmail.com>
2024-03-24 03:19:08 -0400

    bkp

--- tests/utils_test.go
+++ tests/utils_test.go
@@ -5,8 +5,59 @@ import (
 	"path/filepath"
 	"testing"
 
+	"kvmgo/types"
 	"kvmgo/utils"
 )
+
+func TestFileInterface(t *testing.T) {
+	// Setup: create a new file and directory
+	basePath := "exampleDir"
+	fPath, err := types.NewPath(basePath)
+	if err != nil {
+		t.Errorf("Failed to Resolve New Path to Abs ERROR:%s", err)
+	}
+
+	fPath.PrintPaths()
+
+	absPath, err := fPath.ToAbs()
+	if err != nil {
+		t.Fatalf("Error converting to absolute path: %v", err)
+	}
+
+	if !absPath.Valid() {
+		if err := absPath.CreateFolder(); err != nil {
+			t.Fatalf("Failed to create folder: %v", err)
+		}
+	}
+
+	filePath, err := types.NewPath(filepath.Join(absPath.Get(), "newFile.txt"))
+	if err != nil {
+		t.Errorf("Failed to Resolve Path ERROR:%s", err)
+	}
+	if err := filePath.CreateFile(); err != nil {
+		t.Fatalf("Failed to create file: %v", err)
+	}
+
+	filePath.PrintPaths()
+
+	// // New: demonstrate and verify deletion of file and directory
+	if err := filePath.DeleteFile(); err != nil {
+		t.Errorf("Failed to delete file: %v", err)
+	}
+
+	if err := absPath.DeleteDir(); err != nil {
+		t.Errorf("Failed to delete folder: %v", err)
+	}
+
+	// Verify deletion
+	if filePath.Valid() {
+		t.Error("File still exists after deletion")
+	}
+
+	if absPath.Valid() {
+		t.Error("Directory still exists after deletion")
+	}
+}
 
 func TestCreateDirIfNotExist(t *testing.T) {
 	tmpDir, err := os.MkdirTemp("", "testdir")
@@ -33,3 +84,11 @@ func TestCreateDirIfNotExist(t *testing.T) {
 }
 
 // go test
+// # search "someterm"
+// alias search='rg --column --line-number --no-heading --color=always --smart-case "$@" | fzf --ansi --multi --reverse --preview "bat --color=always --style=numbers --line-range=:500 {}"'
+
+// # interactive search
+// alias searchi='rg --column --line-number --no-heading --color=always --smart-case | fzf --ansi --multi --reverse --preview "bat --color=always --style=numbers --line-range=:500 {}"'
+
+// git status
+// git restore tests/config_test.go tests/discovery_test.go tests/kafka_test.go tests/keygen_test.go tests/logger_test.go tests/network_test.go tests/p
