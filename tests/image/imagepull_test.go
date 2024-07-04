ba01010084bf940363d907a07614141cf9529cfd
kuro337 <cb.kalz@gmail.com>
2024-07-03 23:39:08 -0400

    kubeauto

---
+++ tests/image/imagepull_test.go
@@ -0,0 +1,11 @@
+package image_test
+
+import (
+	"testing"
+)
+
+func TestPullImg(t *testing.T) {
+	// url := "https://cloud-images.ubuntu.com/releases/noble/release/ubuntu-24.04-server-cloudimg-amd64.img"
+
+	// utils.PullImage(url)
+}
