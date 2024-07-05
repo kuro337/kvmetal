1c6eb837ad40f01f3e207a367bee4e62440c89a4
kuro337 <cb.kalz@gmail.com>
2024-07-05 02:10:16 -0400

    kubeauto new

---
+++ lib/api/test/api_test.go
@@ -0,0 +1,47 @@
+package test
+
+import (
+	"log"
+	"strings"
+	"testing"
+
+	"kvmgo/lib/api"
+
+	"libvirt.org/go/libvirt"
+)
+
+func TestImageApi(t *testing.T) {
+	base := "ubuntu"
+
+	conn, err := libvirt.NewConnect("qemu:///system")
+	if err != nil {
+		log.Printf("Error Connecting %s", err)
+		t.Errorf("Error:%s", err)
+	}
+
+	if !api.CheckPoolExists(conn, base) {
+		t.Fatalf("Pool does not exist")
+	}
+
+	path, err := api.GetPoolPath(conn, base)
+	if err != nil {
+		t.Fatal("Failed to get Path Error:%s", err)
+	}
+
+	imgs, err := api.ListPoolVolumes(conn, path)
+	if err != nil {
+		t.Fatal("Failed to get Path Error:%s", err)
+	}
+
+	if imgs == nil || len(imgs) == 0 {
+		t.Errorf("No Images Present")
+	}
+
+	t.Logf("Volumes: %s\n", strings.Join(imgs, "\n"))
+
+	// Pool exists
+
+	// get path
+
+	// get volumes
+}
