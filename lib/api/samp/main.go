08064e0a0f6b6d5bfb4fe4e3566405b850247b0f
kuro337 <cb.kalz@gmail.com>
2024-07-05 04:52:44 -0400

    kubeauto new

---
+++ lib/api/samp/main.go
@@ -0,0 +1,25 @@
+package samp
+
+import (
+	"log"
+
+	"kvmgo/lib/api"
+)
+
+func main() {
+	name := "testTemp"
+	path := "/home/kuro/testtemp"
+	vm, err := api.NewVM(name, path)
+	if err != nil {
+		log.Fatalf("Error new VM: %s\n", err)
+	}
+
+	url := "https://cloud-images.ubuntu.com/releases/noble/release/ubuntu-24.04-server-cloudimg-amd64.img"
+
+	tmp, err := vm.AddImageHttp(url, "someVM")
+	if err != nil {
+		log.Printf("Error new VM: %s\n", err)
+	}
+
+	log.Printf("Generated : %s\n", tmp)
+}
